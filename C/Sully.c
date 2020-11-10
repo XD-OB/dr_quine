fa0901d41ce2ef842ea5f53001f5bb8fbb6c7b87
XD-OB <ob-96@hotmail.com>
2020-11-10 18:14:43 +0100

    Add Sully

--- C/Sully.c
+++ C/Sully.c
@@ -1,20 +1,34 @@
-int		i = 5;
-
-#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define OPEN(f) open(f, O_WRONLY | O_CREAT, 0644)
+
+int		i = 5;
 
 /*
 	Sully Fully Mully Tully
 */
 
-int	main() {
-	char	*sully = "int%2$c%2$ci = %4$d;%1$c%1$c#include <stdlib.h>%1$c#include <stdio.h>%1$c#include <fcntl.h>%1$c%1$c/*%1$c%2$cSully Fully Mully Tully%1$c*/%1$c%1$cint%2$cmain() {%1$c%2$cchar%2$c*sully = %3$c%5$s%3$c;%1$c%1$c%2$cwhile (i-- > 0) {%1$c%2$c%2$cchar%2$cfilename[10];%1$c%2$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%2$c%2$cdprintf(open(filename, O_WRONLY | O_CREAT, 0644), sully, 10, 9, 34, i, sully);%1$c%2$c}%1$c%2$creturn 0;%1$c}%1$c";
+void	compile() {
+	if (i > 0) {
+		char	cmd[51];
+		sprintf(cmd, "gcc -Wall -Wextra -Werror ./Sully_%1$d.c -o ./Sully_%1$d", i);
+		system(cmd);
+		sprintf(cmd, "./Sully_%1$d", i);
+		system(cmd);
+	}
+}
 
-	while (i-- > 0) {
+int	main() {
+	if (i >= 0) {
+		if (access("./Sully_5.c", R_OK)) i--;
+		char	*sully = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$c%1$c#define OPEN(f) open(f, O_WRONLY | O_CREAT, 0644)%1$c%1$cint%2$c%2$ci = %4$d;%1$c%1$c/*%1$c%2$cSully Fully Mully Tully%1$c*/%1$c%1$cvoid%2$ccompile() {%1$c%2$cif (i > 0) {%1$c%2$c%2$cchar%2$ccmd[51];%1$c%2$c%2$csprintf(cmd, %3$cgcc -Wall -Wextra -Werror ./Sully_%%1$d.c -o ./Sully_%%1$d%3$c, i);%1$c%2$c%2$csystem(cmd);%1$c%2$c%2$csprintf(cmd, %3$c./Sully_%%1$d%3$c, i);%1$c%2$c%2$csystem(cmd);%1$c%2$c}%1$c}%1$c%1$cint%2$cmain() {%1$c%2$cif (i >= 0) {%1$c%2$c%2$cif (access(%3$c./Sully_5.c%3$c, R_OK)) i--;%1$c%2$c%2$cchar%2$c*sully = %3$c%5$s%3$c;%1$c%2$c%2$cchar%2$cfilename[10];%1$c%2$c%2$csprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%2$c%2$cdprintf(OPEN(filename), sully, 10, 9, 34, i, sully);%1$c%2$c%2$ccompile();%1$c%2$c}%1$c%2$creturn 0;%1$c}%1$c";
 		char	filename[10];
 		sprintf(filename, "Sully_%d.c", i);
-		dprintf(open(filename, O_WRONLY | O_CREAT, 0644), sully, 10, 9, 34, i, sully);
+		dprintf(OPEN(filename), sully, 10, 9, 34, i, sully);
+		compile();
 	}
 	return 0;
 }
