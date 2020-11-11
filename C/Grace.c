6025fb0a508cf4b356ae527a4e386be2de6cdbf8
XD-OB <ob-96@hotmail.com>
2020-11-11 04:39:37 +0100

    Fix Makefile

--- C/Grace.c
+++ C/Grace.c
@@ -6,7 +6,7 @@
 */
 
 #define OPEN(file) open(file, O_WRONLY | O_CREAT, 0644)
-#define GRACE "#include <fcntl.h>%2$c#include <stdio.h>%2$c%2$c/*%2$c%3$cMy little Grace%2$c*/%2$c%2$c#define OPEN(file) open(file, O_WRONLY | O_CREAT, 0644)%2$c#define GRACE %1$c%4$s%1$c%2$c#define FT(x) int main() { dprintf(OPEN(%1$cGrace_kid.c%1$c), x, 34, 10, 9, x); return 0 }%2$c%2$cFT(GRACE)%2$c"
-#define FT(x) int main() { dprintf(OPEN("Grace_kid.c"), x, 34, 10, 9, x); return 0 }
+#define GRACE "#include <fcntl.h>%2$c#include <stdio.h>%2$c%2$c/*%2$c%3$cMy little Grace%2$c*/%2$c%2$c#define OPEN(file) open(file, O_WRONLY | O_CREAT, 0644)%2$c#define GRACE %1$c%4$s%1$c%2$c#define FT(x) int main() { dprintf(OPEN(%1$cGrace_kid.c%1$c), x, 34, 10, 9, x); return 0; }%2$c%2$cFT(GRACE)%2$c"
+#define FT(x) int main() { dprintf(OPEN("Grace_kid.c"), x, 34, 10, 9, x); return 0; }
 
 FT(GRACE)
